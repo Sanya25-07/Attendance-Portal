1f0ec08eb77c96358469c199d266b51bc721e762
Sanya25-07 <63836959+Sanya25-07@users.noreply.github.com>
2021-03-13 15:49:18 +0530

    Added Sidebar.php with site.css

---
+++ Sidebar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Side Menu Bar CSS</title>
+    <link rel="stylesheet" href="sidebar.css">
+</head>
+<body>
+    <div class="sidebar">
+        <header>ADMIN MENU</header>
+    <ul>
+        <li><a href="ViewProfile.php">View Profile</a></li>
+        <li><a href="AddStudent.php">Add Student</a></li>
+        <li><a href="#">Add Teacher</a></li>
+        <li><a href="#">Add Staff</a></li>
+        <li><a href="#">Reports</a></li>
+        <li><a href="#">Feedback</a></li>
+    </ul>
+    </div>
+</body>
+</html>
